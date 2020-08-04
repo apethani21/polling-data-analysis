877cdea9367225c31339dcf2fabf26034316efa7
apethani21 <ajaypethani97@gmail.com>
2020-08-04 23:17:03 +0100

    script to update data and notebook

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+printf "running extract_britain_elects.py\n"
+/home/ubuntu/anaconda3/envs/email-service/bin/python extract_britain_elects.py;
+printf "executing analyse_polling_data.ipynb..\n";
+/home/ubuntu/anaconda3/envs/email-service/bin/jupyter nbconvert \
+    --to notebook \
+    --execute \
+    /home/ubuntu/repo/polling-data-analysis/analyse_polling_data.ipynb && \
+    printf "successfully executed analyse_polling_data.ipynb\n" && \
+    mv /home/ubuntu/repo/polling-data-analysis/analyse_polling_data.nbconvert.ipynb \
+    /home/ubuntu/repo/polling-data-analysis/analyse_polling_data.ipynb && \
+    printf "analyse_polling_data.ipynb updated\n";
